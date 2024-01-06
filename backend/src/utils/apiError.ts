4e33d4f6e736088d2a9d1ff61b3ef889b23c1846
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-01-06 18:37:45 +0530

    add: apiError util

---
+++ backend/src/utils/apiError.ts
@@ -0,0 +1,30 @@
+interface ApiErrorProps {
+    statusCode: number;
+    message? : string;
+    errors?: any [];
+    stack? : string;
+}
+
+class ApiError extends Error{
+
+    public statusCode: number;
+    public data : null | any;
+    public message: string;
+    public errors: any[];
+    public success: boolean;
+
+    constructor({ statusCode, message = 'something went wrong', errors = [], stack }: ApiErrorProps){
+        super(message)
+        this.statusCode = statusCode,
+        this.data = null,
+        this.message = message,
+        this.errors = errors,
+        this.success = false
+
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}

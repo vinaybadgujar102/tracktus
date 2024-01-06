09a56a0fbce81f95b163b4c6762b7b0b12326963
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-01-06 18:20:37 +0530

    refactor: express app initialization

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db";
+import app from "./app";
 
 dotenv.config();
 
-connectDB();
+connectDB()
+.then( () => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Listening on port ${process.env.PORT || 8000}`);
+  });
+})
+.catch((err) => {
+  console.log("MongoDB connection failed!!! ", err);
+});
